e1c73fd6d4cefe96021a5e519616d6d9e3c00d74
scottpschaffer <scottpschaffer@yahoo.com>
2017-05-30 19:38:00 -0500

    Finished Encode and added Decode. Created som unit tests.

--- First/Cipher.Tests/UnitTest1.cs
+++ First/Cipher.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
-
+using Cipher;
+ 
 namespace Cipher.Tests
 {
     public class UnitTest1
@@ -8,7 +9,55 @@ namespace Cipher.Tests
         [Fact]
         public void Test1()
         {
-
+            Assert.Equal("HM1QA", Program.Encode("He1lo"));
+        }
+ 
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal("HE1LO", Program.Decode("HM1QA"));
+        }
+ 
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal("5DDC4DDF-7A47-44D3-8EDA-3171860DC938", Program.Decode("5DHG4GHJ-7G47-44E3-8IIE-3171860EG938"));
+        }
+ 
+        [Fact]
+        public void Test4()
+        {
+            Assert.Equal("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Program.Decode("TBM VLDLN mTGLK TUM HEHCI HKAW LBM QMAY CSV"));
+        }
+ 
+        [Fact]
+        public void Test5()
+        {
+            Assert.Equal("5DHG4GHJ-7G47-44E3-8IIE-3171860EG938", Program.Encode("5ddc4ddf-7a47-44d3-8eda-3171860dc938"));
+        }
+ 
+        [Fact]
+        public void Test6()
+        {
+            Assert.Equal("TBM VLDLN MTGLK TUM HEHCI HKAW LBM QMAY CSV", Program.Encode("The quick brown fox jumps over the lazy dog"));
+        }
+ 
+        [Fact]
+        public void Test7()
+        {
+            Assert.Equal("5115D0E8-BF6I-40H6-80I3-34K2GE2E9185", Program.Encode("5115D0A8-AE6D-40D6-80E3-34F2AD2A9185"));
+        }
+ 
+        [Fact]
+        public void Test8()
+        {
+            Assert.Equal(" ", Program.Encode(" "));
+        }
+ 
+        [Fact]
+        public void Test9()
+        {
+            Assert.Equal("", Program.Encode(""));
         }
     }
 }
--- First/Cipher/Cipher.cs
+++ First/Cipher/Cipher.cs
@@ -2,53 +2,153 @@
 
 namespace Cipher
 {
-    class Program
+    public class Program
     {
 
         public static string Encode(string input)
         {
-            // Convert chars to Uppercase letters
-            string input2 = input.ToUpper();
-            // Convert to char array
-            char[] convert1 = input.ToCharArray();
-            // sub is used if a non-alphabetic char is parsed, then skip over it
-            int sub = 1;
-            // While there are chars in array start with 2nd char
-
-            for (var a = 1; a < (convert1.Length - 1); a++)
+            if ((input != "") && (input != null))
             {
-                // Get ASCII value of char
-                int y = (int)convert1[a];
-                // If non-alphabetic then increase sub and skip over
-                if ((y < 65) || (y > 90))
+                // Char array to store new String
+                char[] newString = new char[input.Length];
+                // Convert chars to Uppercase letters
+                string input2 = input.ToUpper();
+                // Convert to char array
+                char[] convert1 = input2.ToCharArray();
+                // sub is used if a non-alphabetic char is parsed, then skip over it
+                int sub = 1;
+                int letter = 0;
+                while (((convert1[letter] < 65) && (convert1[letter] > 32)) || (convert1[letter] > 90))
                 {
-                    sub++;
-                    continue;
+                    newString[letter] = convert1[letter];
+                    letter++;
                 }
-                else
+ 
+                newString[letter] = convert1[letter++];
+                // While there are chars in array start with 2nd char
+                for (var a = letter; a < convert1.Length; a++)
                 {
-                    // Get number value of character
-                    int numChar = (int)convert1[a];
-                    // z is the ASCII value of previous letter
-                    int z = ((int)convert1[a - sub] % 65);
-                    // If new ASCII value is greater than Z
-                    if ((numChar + z) > 90)
+                    // Get ASCII value of char
+                    int y = (int)convert1[a];
+                    // If non-alphabetic then increase sub and skip over
+                    if ((y < 65) || (y > 90))
                     {
-                        // Get new value of numChar
-                        numChar = ((numChar % 90) + 64);
+                        sub++;
+                        newString[a] = (char)y;
+                        continue;
                     }
-                    // Assign new value of numChar to char array
-                    convert1[a] = (char)numChar;
-                    // reset sub back to 1
-                    sub = 1;
+                    else
+                    {
+                        // Get number value of character
+                        //int numChar = (int)convert1[a];
+                        // z is the ASCII value of previous letter
+                        int z = ((int)convert1[a - sub] % 64);
+                        // If new ASCII value is greater than Z
+                        int w = y + z;
+                        if (w > 90)
+                        {
+                            // Get new value of numChar
+                            w = ((w % 90) + 64);
+                        }
+                        // Assign new value of numChar to char array
+                        newString[a] = (char)w;
+                        // reset sub back to 1
+                        sub = 1;
+                    }
+                   
                 }
                
+                return new string(newString);
+            }
+            else
+            {
+                return string.Empty;
             }
         }
+ 
+        public static string Decode(string input)
+        {
+            if ((input != "") && (input != null))
+            {
+                // Char array to store new String
+                char[] newString = new char[input.Length];
+                // Convert chars to Uppercase letters
+                string input2 = input.ToUpper();
+                // Convert to char array
+                char[] convert1 = input2.ToCharArray();
+                // sub is used if a non-alphabetic char is parsed, then skip over it
+                int sub = 1;
+                int letter = 0;
+                while (((convert1[letter] < 65) && (convert1[letter] > 32)) || (convert1[letter] > 90))
+                {
+                    letter++;
+                }
+               
+                for (var a = ++letter; a < convert1.Length; a++)
+                {
+                    // Get ASCII value of char
+                    int y = (int)convert1[a];
+                    //Console.WriteLine(y.ToString());
+                    // If non-alphabetic then increase sub and skip over
+                    if ((y < 65) || (y > 90))
+                    {
+                        sub++;
+                        convert1[a] = (char)y;
+                        continue;
+                    }
+                    else
+                    {
+                        // Get number value of character
+                        //int numChar = (int)convert1[a];
+                        // z is the ASCII value of previous letter
+                        int z = ((int)convert1[a - sub] % 64);
+                        // If new ASCII value is greater than Z
+                        int w = y - z;
+                        if (w < 65)
+                        {
+                            // Get new value of numChar
+                            w = (91 - (65 - w));
+                        }
+                        // Assign new value of numChar to char array
+                        convert1[a] = (char)w;
+                        // reset sub back to 1
+                        sub = 1;
+                    }
+                   
+                }
+
+                return new string(convert1);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }   
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string x0 = Encode("");
+ 
+            string x0a = Encode(null);
+ 
+            string x1 = Encode("He1lo");
+            Console.WriteLine(x1);
+ 
+            string x2 = Decode("5DHG4GHJ-7G47-44E3-8IIE-3171860EG938");
+            Console.WriteLine(x2);
+            Console.WriteLine("5DDC4DDF-7A47-44D3-8EDA-3171860DC938");
+ 
+            string x3 = Encode("5DDC4DDF-7A47-44D3-8EDA-3171860DC938");
+            Console.WriteLine(x3);
+            Console.WriteLine("5DHG4GHJ-7G47-44E3-8IIE-3171860EG938");
+ 
+            string x4 = Encode("The quick brown fox jumps over the lazy dog");
+            Console.WriteLine(x4);
+            Console.WriteLine("TBM VLDLN MTGLK TUM HEHCI HKAW LBM QMAY CSV");
+ 
+            string x5 = Decode("TBM VLDLN mTGLK TUM HEHCI HKAW LBM QMAY CSV");
+            Console.WriteLine(x5);
+            Console.WriteLine("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
         }
     }
 }
