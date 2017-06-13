0790ffbc4d35cc4ac7c81a37f83bf33a4bab43d2
scottpschaffer <scottpschaffer@yahoo.com>
2017-06-12 21:13:54 -0500

    Submitted Code and almost all Tests

--- Second/DayCount.Tests/UnitTest1.cs
+++ Second/DayCount.Tests/UnitTest1.cs
@@ -9,25 +9,28 @@ namespace DayCount.Tests
         public void Test1_2DaysAgo()
         {
             Day TwoDaysAgo = new Day();
-            Assert.Equal("The day is in the past!", TwoDaysAgo.countDays(new DateTime(2017,6,5)));
+            Assert.Equal("The day is in the past!", TwoDaysAgo.countDays(DateTime.Now - (new TimeSpan(48, 0, 0))));
         }
 
         [Fact]
         public void Test2_1DayAgo()
         {
-
+            Day OneDayAgo = new Day();
+            Assert.Equal("The day is in the past!", OneDayAgo.countDays(DateTime.Now - (new TimeSpan(24, 0, 0))));
         }
 
         [Fact]
         public void Test3_SameDay()
         {
-
+            Day Today = new Day();
+            Assert.Equal("Today is the day!", Today.countDays(DateTime.Now));
         }
 
         [Fact]
         public void Test4_1DayAhead()
         {
-
+            Day Tomorrow = new Day();
+            Assert.Equal("1 days", Tomorrow.countDays(DateTime.Now + (new TimeSpan(24, 0, 0))));
         }
 
         [Fact]
--- Second/DayCount/DayCount.cs
+++ Second/DayCount/DayCount.cs
@@ -7,12 +7,11 @@ namespace DayCount
 
         public string countDays(DateTime d)
         {
+            d += new TimeSpan(23, 59, 0);
             DateTime today = DateTime.Now;
-            Console.WriteLine("One" + (d - today));
             TimeSpan difference = (d - today);
-            TimeSpan newDifference;
-            Console.WriteLine("Difference" + difference);
-            if ( difference.Days < 0)
+
+            if ((difference.Days <= 0) && ((difference.Hours < 0) || (difference.Minutes < 0) || (difference.Seconds < 0) || (difference.Milliseconds < 0)))
             {
                 return "The day is in the past!";
             }
@@ -22,18 +21,14 @@ namespace DayCount
             }
             else
             {
-                if ((difference.Hours > 0) || (difference.Minutes > 0) || (difference.Seconds > 0) || (difference.Milliseconds > 0))
-                {
-                    newDifference = difference + new TimeSpan(24, 0, 0);
-                }
-                return newDifference.Days + " days";
+                return difference.Days + " days";
             }
         }
 
         static void Main(string[] args)
         {
             Day day1 = new Day();
-            Console.WriteLine(day1.countDays(new DateTime(2017,6,17)));
+            Console.WriteLine(day1.countDays(new DateTime(2017,6,13)));
         }
     }
 }
